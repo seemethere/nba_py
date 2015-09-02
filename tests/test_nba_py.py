e7665bc91d5e0f0ccaf61d7a89c39ac8940deacd
Eli <fez1452@gmail.com>
2015-09-02 17:10:08 -0500

    Update tests to fit in with Travis CI

--- tests/test_nba_py.py
+++ tests/test_nba_py.py
@@ -1,5 +1,4 @@
 import nba_py
 # from pandas import DataFrame
-
-sc = nba_py.Scoreboard(month=2, day=21, year=2015)
-print sc.game_header()
+def test():
+    assert nba_py.Scoreboard(month=2, day=21, year=2015)
