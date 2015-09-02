3b5941ad3549d89cb5062376956f191fec79fa4e
Eli <fez1452@gmail.com>
2015-09-02 16:07:43 -0500

    Nosetest implementation for players file

--- tests/test_nba_py_player.py
+++ tests/test_nba_py_player.py
@@ -1,13 +1,27 @@
 from nba_py import player
+from nba_py.player import get_player
 
-pd = player.PlayerDashboard('203507')
-print pd.starting_position()
 
-ap = player.CommonAllPlayers()
-print ap.info()
-
-pc = player.PlayerInfoCommon('203507')
-print pc.headline_stats()
-
-p_cstats = player.PlayerCareerStats('201939')
-print p_cstats.regular_season_career_totals()
+def test():
+    pid = get_player('Tim', 'Duncan')
+    vs_pid = get_player('Stephen', 'Curry')
+    assert player.PlayerList()
+    assert player.PlayerSummary(pid)
+    assert player.PlayerGeneralSplits(pid)
+    assert player.PlayerOpponentSplits(pid)
+    assert player.PlayerLastNGamesSplits(pid)
+    assert player.PlayerInGameSplits(pid)
+    assert player.PlayerClutchSplits(pid)
+    assert player.PlayerShootingSplits(pid)
+    assert player.PlayerPerformanceSplits(pid)
+    assert player.PlayerYearOverYearSplits(pid)
+    assert player.PlayerCareer(pid)
+    assert player.PlayerProfile(pid)
+    assert player.PlayerGameLogs(pid)
+    assert player.PlayerShotTracking(pid)
+    assert player.PlayerReboundTracking(pid)
+    assert player.PlayerPassTracking(pid)
+    assert player.PlayerDefenseTracking(pid)
+    assert player.PlayerShotLogTracking(pid)
+    assert player.PlayerReboundLogTracking(pid)
+    assert player.PlayerVsPlayer(pid, vs_pid)
