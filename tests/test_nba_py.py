dc63c1aec4dc9b857b1733778b6e446ad1320ffe
Eli <fez1452@gmail.com>
2015-09-03 09:38:21 -0500

    Add some proper tests to both base and game

--- tests/test_nba_py.py
+++ tests/test_nba_py.py
@@ -1,4 +1,4 @@
 import nba_py
-# from pandas import DataFrame
+
 def test():
     assert nba_py.Scoreboard(month=2, day=21, year=2015)
--- tests/test_nba_py_game.py
+++ tests/test_nba_py_game.py
@@ -1,2 +1,11 @@
+from nba_py import game
+
 def test():
-    assert True
+    gid = '0041400122'
+    assert game.Boxscore(gid)
+    assert game.BoxscoreScoring(gid)
+    assert game.BoxscoreUsage(gid)
+    assert game.BoxscoreMisc(gid)
+    assert game.BoxscoreAdvanced(gid)
+    assert game.BoxscoreFourFactors(gid)
+    assert game.PlayByPlay(gid)
